28adbb0344c6318cfffd8b2b8ead032f6bd27c06
Dusan Vostic <vosticdusan@gmail.com>
2021-02-16 23:27:01 +0100

    Django migrations and models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class ToDo(models.Model):
+  PRIORITY_TYPES = [
+    ('L', 'Low'),
+    ('M', 'Medium'),
+    ('H', 'High')
+  ]
+  title = models.CharField(max_length = 128)
+  priority = models.CharField(
+    max_length = 1,
+    choices = PRIORITY_TYPES)
+  owner = models.CharField(max_length = 128)
+
+  def __str__(self):
+    return self.title
